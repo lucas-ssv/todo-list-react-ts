e59c9310414b745b02a20eb2d73f0a3602325346
Lucas Vieira <lucas_99silva@hotmail.com>
2023-06-07 16:11:01 -0300

    feat: ensure Checkbox checks on click

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,10 +1,19 @@
-import { Circle } from '@phosphor-icons/react'
+import { Circle, CheckCircle } from '@phosphor-icons/react'
 import styles from './Checkbox.module.css'
+import { ButtonHTMLAttributes } from 'react'
 
-export function Checkbox () {
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isChecked?: boolean
+}
+
+export function Checkbox ({ isChecked = false, ...props }: Props) {
   return (
-    <button className={styles.checkbox}>
-      <Circle size={24} />
+    <button className={styles.checkbox} {...props}>
+      {isChecked ? (
+        <CheckCircle size={24} weight='fill' color='#5E60CE' />
+      ) : (
+        <Circle size={24} />
+      )}
     </button>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import { Trash } from '@phosphor-icons/react'
 import { Checkbox } from './Checkbox'
 import styles from './Task.module.css'
 
 export function Task () {
+  const [isCheckedTask, setIsCheckedTask] = useState<boolean>()
+
+  function handleCheckTask () {
+    setIsCheckedTask(isCheckedTask => !isCheckedTask)
+  }
+
   return (
     <div className={styles.task}>
-      <Checkbox />
+      <Checkbox onClick={handleCheckTask} isChecked={isCheckedTask} />
       <p>
         Integer urna interdum massa libero auctor neque turpis turpis semper.
         Duis vel sed fames integer.
